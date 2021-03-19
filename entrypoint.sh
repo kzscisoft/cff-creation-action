2cd4e717cbff3b5611bf267fea39627571833c0a
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2021-03-19 08:21:37 +0000

    Try arguments in array

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ COMMIT_URL="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REP
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 
-ARGUMENTS="--repo-url ${REMOTE_URL}"
+ARGUMENTS=("--repo-url ${REMOTE_URL}")
 
 if [ -n "${INPUT_BRANCH}" ]
 then
@@ -26,32 +26,32 @@ fi
 
 if [ -n "${INPUT_DOI}" ]
 then
-    ARGUMENTS="${ARGUMENTS} --doi \"${INPUT_DOI}\""
+    ARGUMENTS+=("--doi \"${INPUT_DOI}\"")
 fi
 
 if [ -n "${INPUT_TITLE}" ]
 then
-    ARGUMENTS="${ARGUMENTS} --title \"${INPUT_TITLE}\""
+    ARGUMENTS+=("--title \"${INPUT_TITLE}\"")
 fi
 
 if [ -n "${INPUT_MESSAGE}" ]
 then
-    ARGUMENTS="${ARGUMENTS} --message \"${INPUT_MESSAGE}\""
+    ARGUMENTS+=("--message \"${INPUT_MESSAGE}\"")
 fi
 
 if [ -n "${INPUT_AUTHORS}" ]
 then
-    ARGUMENTS="${ARGUMENTS} --authors \"${INPUT_AUTHORS}\""
+    ARGUMENTS+=("--authors \"${INPUT_AUTHORS}\"")
 fi
 
 if [ -n "${INPUT_AFFILIATION}" ]
 then
-    ARGUMENTS="${ARGUMENTS} --affiliation \"${INPUT_AFFILIATION}\""
+    ARGUMENTS+=("--affiliation \"${INPUT_AFFILIATION}\"")
 fi
 
 echo "Running command: gen-cff ${INPUT_PROJECT_PATH} ${ARGUMENTS}"
 
-gen-cff ${INPUT_PROJECT_PATH} ${ARGUMENTS}
+gen-cff ${INPUT_PROJECT_PATH} "${ARGUMENTS[@]}"
 
 if [ ! -f "CITATION.cff" ]
 then
